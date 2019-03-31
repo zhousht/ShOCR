dda0a088255663c3fd37909dd2915ef7b2077f24
zhousht <zhousht@hotmail.com>
2019-03-30 22:55:15 -0600

    Add project files.

---
+++ SZOCREngine/Preprocessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SZOCREngine
+{
+    public static class Preprocessor
+    {
+        public static void Binarization()
+        {
+            ToGray();
+            ToBlackWhite();
+        }
+
+        public static void ToBlackWhite()
+        {
+
+        }
+
+        public static void ToGray()
+        {/*
+            // create filter
+            Threshold filter = new Threshold(100);
+            // apply the filter
+            filter.ApplyInPlace(image);
+            */
+        }
+    }
+}
